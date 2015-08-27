ce33d72fb5ef224aacc7fbbc521651755899ff9b
Jonathan Peck <peckjonathan@gmail.com>
2015-08-27 21:49:26 +0200

    IVs lol

--- controllers/ProjectsController.php
+++ controllers/ProjectsController.php
@@ -55,7 +55,7 @@ class ProjectsController extends Controller
         
         $algo = 'twofish';
         $mode = 'cbc';
-        $iv = 'spastiespastiespastie';
+        $iv = 'spastiespastiesp';
         $hash = 'sha256';
         
         if ($new_form->load(Yii::$app->request->post())) {
