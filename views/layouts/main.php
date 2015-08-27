945afbf3bd086c59056a356b6f017ecd0060d67a
Jonathan Peck <peckjonathan@gmail.com>
2015-08-27 19:41:42 +0200

    Fixed more links

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,7 +4,6 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\helpers\BaseHtml;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
--- views/projects/index.php
+++ views/projects/index.php
@@ -3,6 +3,7 @@
 /* @var $this yii\web\View */
 
 $this->title = 'Pecky\'s projects';
+use yii\helpers\Html;
 ?>
 <div class="site-index">
 
