b79064a2635f7a7e51889ec6436ff64f95386dae
Jonathan Peck <peckjonathan@gmail.com>
2015-08-28 12:55:06 +0200

    pls to work

--- models/NonsenseForm.php
+++ models/NonsenseForm.php
@@ -34,9 +34,20 @@ class NonsenseForm extends Model
     
     public function generate()
     {
-        return http_post_fields('http://linode.pecky.be/cgi-bin/nonsens.py', [
-            'start' => $this->start,
-        ]);
+        $url = 'http://linode.pecky.be/cgi-bin/nonsens.py';
+        $data = array('start' => $this->start);
+
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data),
+            ),
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+
+        return $result;
     }
 
 }
