cff5c8562bd5f0c578289d566838cd0d5880d3e2
Jonathan Peck <peckjonathan@gmail.com>
2015-08-28 13:57:11 +0200

    More interesting about page

--- views/site/about.php
+++ views/site/about.php
@@ -6,13 +6,23 @@ use yii\helpers\Html;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
+$ip = escapeshellcmd($_SERVER['REMOTE_ADDR']);
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <h1>About me</h1>
     <p>
         This is where I'd write something about me.
         However, that's boring, so I haven't gotten around to this yet.
         Stalkers, stay tuned!
+    </p>
+    
+    <h1>About you</h1>
+    <p>
+        Let me tell you a few things about yourself.
+    </p>
+    <p>
+        <strong>Your IP:</strong> <?= $ip; ?>
+        <br>
+        <pre><?= passthru('whois ' . $ip); ?></pre>
     </p>
 </div>
