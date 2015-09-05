2e03614c83004caa08add86cf410a2a8d8088980
Jonathan Peck <peckjonathan@gmail.com>
2015-09-05 17:58:01 +0200

    Subway finished

--- views/projects/subway/index.php
+++ views/projects/subway/index.php
@@ -1,8 +1,35 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* @var $this yii\web\View */
 
+$this->title = 'Subway';
+$this->params['breadcrumbs'][] = $this->title;
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Subway</h1>
+
+        <p class="lead">
+            Create efficient subway networks.
+        </p>
+    </div>
+
+    <div class="body-content">
+        <h2>Define the problem</h2>
+        <?php $form = ActiveForm::begin(['id' => 'subway-form']); ?>
+        
+        <?= $form->field($model, 'size') ?>
+
+        <?= Html::submitButton('Generate solution', ['class' => 'btn btn-primary']) ?>
+        <?php ActiveForm::end(); ?>
+        
+        <h2>View the solution</h2>
+        <?php if(!empty($result)): ?>
+        <img src="data:image/png;base64,<?= $result ?>"/>
+        <?php endif; ?>
+    </div>
+</div>
