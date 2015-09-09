f8a9aa0ccd16591612a3620cbcc5f94025ed92f7
Jonathan Peck <peckjonathan@gmail.com>
2015-09-09 20:33:41 +0200

    Every page has HTTP warning

--- views/site/index.php
+++ views/site/index.php
@@ -13,14 +13,5 @@ use yii\helpers\Html;
         <p class="lead">A place of science and wonder.</p>
 
         <p><?= Html::a('View all projects', ['/projects'], ['class'=>'btn btn-primary']) ?></p>
-        
-        <?php if(!isset($_SERVER['HTTPS'])): ?>
-        <div class="alert alert-danger">
-            I recommend using this site <a href="https://www.pecky.be/">over a secure connection</a>,
-            as some projects on here require you to enter possibly sensitive information. If anyone
-            were to intercept your connection (say, the NSA or your own friendly government), that
-            could completely defeat the purpose of these applications. It might also put you at risk.
-        </div>
-        <?php endif; ?>
     </div>
 </div>
