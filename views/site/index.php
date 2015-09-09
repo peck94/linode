58b1880c36ed2bc3d345e56bd3a517475c661d82
Jonathan Peck <peckjonathan@gmail.com>
2015-09-09 20:30:05 +0200

    HTTPS banner

--- views/site/index.php
+++ views/site/index.php
@@ -13,5 +13,14 @@ use yii\helpers\Html;
         <p class="lead">A place of science and wonder.</p>
 
         <p><?= Html::a('View all projects', ['/projects'], ['class'=>'btn btn-primary']) ?></p>
+        
+        <?php if(!isset($_SERVER['HTTPS'])): ?>
+        <p>
+            I recommend using this site <a href="https://www.pecky.be/">over a secure connection</a>,
+            as some projects on here require you to enter possibly sensitive information. If anyone
+            were to intercept your connection (say, the NSA or your own friendly government), that
+            could completely defeat the purpose of these applications. It might also put you at risk.
+        </p>
+        <?php endif; ?>
     </div>
 </div>
