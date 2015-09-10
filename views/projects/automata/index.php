b33c2dbfe574d829d66c515cc09f4d81ec589a84
Jonathan Peck <peckjonathan@gmail.com>
2015-09-10 15:58:26 +0200

    More docs

--- views/projects/automata/index.php
+++ views/projects/automata/index.php
@@ -25,6 +25,39 @@ $items = [
     </div>
 
     <div class="body-content">        
+        <h2>Explanation</h2>
+        <p>
+            An <em>elementary cellular automaton</em> is basically a row of <em>n</em>
+            bits together with a set of rules for transforming that row into another
+            row of the same length. For any bit <em>i</em>, the rules specify what
+            value <em>i</em> gets by inspecting the current value and the immediate
+            (left and right) neighbors. This means a single rule may be summarized
+            in four bits, as in the table below:
+            <br>
+            <table>
+                <tr>
+                    <th>Left</th>
+                    <th>Current</th>
+                    <th>Right</th>
+                    <th>New</th>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>1</td>
+                    <td>0</td>
+                    <td>1</td>
+                </tr>
+            </table>
+            <br>
+            This rule says that, if the current bit's left neighbor is 0, its right
+            neighbor is 0 and its current value is 1, then its new value should be 1.
+            There are 2<sup>3</sup> = 8 possible states to be examined, and each state
+            requires specifying a single output bit. So one can count that the
+            elementary cellular automata can be summarized by an 8-bit number,
+            or a single byte (i.e. a value from 0 to 255 inclusive). This means
+            there are exactly 256 possible distinct elementary cellular automata.
+        </p>
+        
         <h2>Enter details</h2>
 
         <?php $form = ActiveForm::begin(['id' => 'automata-form']); ?>
